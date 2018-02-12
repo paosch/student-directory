271b9e7f61acfb76664aa9f45e9e85368d9d10a2
paosch <paola_schiaffino@yahoo.co.uk>
2018-02-12 18:36:22 +0000

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,30 +1,32 @@
-#we put students inside an array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do #while name not empty
+    students << {name: name, cohort: :november} #add student hash to the array
+    puts "Now we have #{students.count} students"
+    name = gets.chomp #get another name from the user
+  end
+  students #returns the array
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
 end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 #we now need to call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
